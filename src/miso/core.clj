98955e3129c0265915aa427b08e8aa6545a86f16
David Marrs <david@marrsbytes.com>
2024-03-07 00:04:44 +0000

    Allow sources to be a collection or a string
    Reduces boilerplate in calling functions.

--- src/miso/core.clj
+++ src/miso/core.clj
@@ -4,26 +4,47 @@
              [clojure.java.io :as io]
              [babashka.fs :as fs]))
 
-(defn filter-newer-inputs [target-map]
-  (reduce (fn [acc [target srcs]]
-            (let [target-file (io/file target)]
+(defn mkdirp! [f]
+  (-> f
+      fs/parent
+      fs/path
+      fs/create-dirs))
+
+(defn filter-newer-inputs
+  "Returns a map of targets from sources for those
+  sources that are newer than their target."
+  [target-map]
+  (reduce (fn [acc [target src]]
+            (let [target-file (io/file target)
+                  sources? (coll? src)]
               (if-not (fs/exists? target-file)
-                (assoc acc target-file srcs)
-                (if-let [newer-inputs (seq (fs/modified-since
-                                             target-file
-                                             (->> srcs
-                                                  (map #(.toString %))
-                                                  (map io/file))))]
+                (assoc acc target-file src)
+                (if-let [newer-inputs ((if sources? seq first)
+                                       (fs/modified-since
+                                         target-file
+                                         (let [src (if sources?
+                                                     src (list src))]
+                                           (->> src
+                                                (map str)
+                                                (map io/file)))))]
                   (assoc acc target-file newer-inputs)
                   acc))))
           {}
           target-map))
 
 (defn target<-source
-  "Return a one-to-one map of target file to source file.
+  "Returns a one-to-one map of target file to source file.
   Target filename is generated from source file using f."
   [f sources]
-  (reduce #(assoc %1 (f %2) [%2]) {} sources))
+  (reduce #(assoc %1 (f %2) %2) {} sources))
+
+(defn targets<-source
+  "Returns a map of target files to source file.
+  Target filenames are generated using f, which generates
+  a collection of target files from a source file"
+  [f source]
+  (reduce (fn [acc s] (assoc acc (map #(% s) f) s)) {} sources))
+
 
 (defn rule
   "Given a map of targets to sources and a function (f [m a]),
