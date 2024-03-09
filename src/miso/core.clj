e44694ac50c0092b6115988fc36a5162e194c8da
David Marrs <david@marrsbytes.com>
2024-03-09 17:29:36 +0000

    Replace rule with a pair of chainable runners
    The run!* functions can take a list of source files and produce a list
    of target files.  These functions can be chained together to produce a
    build pipeline.
    
    These functions have replaced rule.

--- src/miso/core.clj
+++ src/miso/core.clj
@@ -6,11 +6,10 @@
 
 (defn mkdirp! [f]
   (-> f
-      fs/parent
       fs/path
       fs/create-dirs))
 
-(defn filter-newer-inputs
+(defn filter-changeset
   "Returns a map of targets from sources for those
   sources that are newer than their target."
   [target-map]
@@ -36,25 +35,43 @@
   "Returns a one-to-one map of target file to source file.
   Target filename is generated from source file using f."
   [f sources]
-  (reduce #(assoc %1 (f %2) %2) {} sources))
+  (reduce #(assoc %1
+                  (if (function? f) (f %2) f)
+                  %2)
+          {} sources))
 
 (defn targets<-source
   "Returns a map of target files to source file.
   Target filenames are generated using f, which generates
   a collection of target files from a source file"
-  [f source]
-  (reduce (fn [acc s] (assoc acc (map #(% s) f) s)) {} sources))
+  [fv sources]
+  (map #(apply target<-source [% sources]) fv))
 
+(defn for-changeset [target-map f]
+  "Iterates over newer sources. Returns sequence of target files."
+  (let [newer (filter-changeset target-map)]
+    (run! f newer target-map)
+    (keys newer)))
 
-(defn rule
-  "Given a map of targets to sources and a function (f [m a]),
-  return (f m a) where m is a modified map containing only
-  targets with newer sources, and a is the original map.
+(defn run!source->target
+  "Generates an injective map of targets to sources using mapper
+  and then processes it with proc!.
+
+  Returns the complete list of target files generated"
+  [sources mapper proc!]
+  (let [target-map (target<-source mapper sources)
+        changeset (filter-changeset target-map)]
+    (proc! changeset target-map)
+    (keys target-map)))
+
+(defn run!source->targets
+  "coll is an array of [mapper proc!] pairs. This is used to
+  generate a set of injective maps of targets to sources.  They
+  are then processed with proc!
   
-  If none of the sources belonging to a target are newer than the target,
-  the entire entry will be omitted from the map."
-  [target-map f]
-  (let [changeset (filter-newer-inputs target-map)]
-    ; TODO: If any of the inputs have deps that are newer than target,
-    ; filter those inputs in as well.
-    (f changeset target-map)))
+  A list of targets, grouped by mapper, is provided to f, for
+  final reduction to a single list of targets to be returned."
+  [sources coll f]
+  (->> coll
+       (mapv #(apply run!source->target (concat [sources] %)))
+       f))
